8e86e92092c0734bf786862143c7a994a0edc1a4
steveriffraff <steve@studioriffraff.com>
2010-08-05 16:57:25 +0100

    Added new templates and styles. Added content to the site and copied the styles across.

--- site/_assets/js/application.js
+++ site/_assets/js/application.js
@@ -11,32 +11,20 @@
  		mainNav: function() {
 			// CONFIG
 			var c = {
-				'speed'		: 400,
-				'ease'		: 'easeOutExpo',
-				'navMain'	: this,
-				'navMainH'	: $(this).children('ul').height(),
-				'navSub'	: '.navSub',
-				'header'	: $('header'),
-				'headerH'	: $('header').height()	
+				'speed'			: 400,
+				'ease'			: 'easeOutExpo',
+				'navMain'		: this,
+				'navMainH'		: $(this).height(),
+				'navSub'		: '.navSub',
+				'header'		: $('header'),
+				'headerH'		: $('header').height(),
+				'navMainItems'	: $(this).children('ul').children('li')
 			};
-			c.navMainItems = c.navMain.children('ul').children('li');
 			
-
 			//Iterate over the current set of matched elements
     		return this.each(function() {
-				// SETUP
-				// 1. Store heights of sub nav for later. Set id's to reference these heights
-				c.navSubHeights = []
-				var i = 0;
-				c.navMainItems.each( function() {
-					var h  = $(this).children(c.navSub).height();
-					$(this).attr('id', 'nM_'+i);
-					if (h) c.navSubHeights.push(h);
-					else   c.navSubHeights.push(0);	
-					i++;
-				})
-				
-				// METHODS
+
+			// METHODS
 				// 1. Toggle Sub Nav Visibility
 				var toggleSubNav = function()	{
 					var thisNavSub   = $('#'+clickedId).children(c.navSub);	
@@ -51,44 +39,49 @@
 				
 				// 2. Toggle Main Nav Active State 
 				var toggleMainNavActiveState = function()	{
-					activeId = $('#'+clickedId);
-					c.navMainItems.removeClass('active');
-					activeId.addClass('active');
+					c.navMainItems.removeClass('active').children('strong').children('a').unwrap();
+					$('#'+clickedId).addClass('active').children('a').wrap('<strong></strong>');
 					
 				}
 				
 				// 3. Toggle Main Nav Height 
-				var toggleHeaderHeight = function(h, timer, speed)	{
+				var toggleHeaderHeight = function(h, delay, speed)	{
 					
-					if ( !speed ) var speed = c.speed*2
+					if (speed) speed = c.speed*2
 					
 					// Fire
-					if ( timer ) {
-						clearTimeout(timer);
-						timer = null;
-					} else {
-						animateHeader();
-					}	
+					if (delay) {
+						setTimeout(function() {
+							if (canCollapseHeader) animateHeader();
+		                }, delay);
+						
+					} else animateHeader();
 			
 					// Methods
 					function animateHeader() { 
 						c.header.stop().animate({'height' : parseInt(h)}, speed, c.ease);
 					}
-					timer = setTimeout(function() {
-						if ( canCollapseHeader ) animateHeader();
-	                }, speed);
 				}
 				
+			// SETUP
+				// 1. Store heights of sub nav for later. Set id's to reference these heights. Store Active Id
+				c.navSubHeights = []
+				var i = 0;
+				c.navMainItems.each( function() {
+					var h  = $(this).children(c.navSub).height();
+					$(this).attr('id', 'nM_'+i);
+					if ($(this).hasClass('active')) c.activeItemNo = $('#nM_'+i)
+					if (h) c.navSubHeights.push(h);
+					else   c.navSubHeights.push(0);	
+					i++;
+				})
 				
-				
-				
-				// FIRE
+			// FIRE
 				var canCollapseHeader = true;
-				toggleHeaderHeight(c.navMainH, true, 0);
+				c.header.css('height' , c.navMainH+(c.activeItemNo.find(c.navSub).height()));
+				toggleHeaderHeight(c.navMainH, c.speed*5);
 				
-				
-				
-				// EVENTS 
+			// EVENTS 
 				// 1. Click
 				c.navMainItems.click(function()	{
 					// variables are used throughout the pluggin
@@ -97,41 +90,28 @@
 					
 					// fire
 					toggleMainNavActiveState();					
-					toggleHeaderHeight(parseInt(thisNavSubH+c.headerH));
+					toggleHeaderHeight(parseInt(thisNavSubH+c.navMainH));
 					return toggleSubNav();
-
 				})
 				
 				// 2. Header: HoverOut - Wait c.time, if user has not Hovered back in close the header
+
 				c.header.hover( function() {
 					canCollapseHeader = false;
 				}, function() {
 					canCollapseHeader = true;
-					toggleHeaderHeight(c.navMainH, true);
+					toggleHeaderHeight(c.navMainH, c.speed*2);
 					
 				})
-			
+
     		});
     	}
 	}
 );
-	
 //pass jQuery to the function, 
 //So that we will able to use any valid Javascript variable name 
 //to replace "$" SIGN. But, we'll stick to $ (I like dollar sign: ) )		
 })(jQuery);
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -142,7 +122,5 @@
 $(document).ready( function()	{
 	$('body').addClass('hasJs');
 	$('#navMain').mainNav(jQuery);
-
-	
 		
 })
